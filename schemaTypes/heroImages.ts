16daf46068bf9892213e7fb3646a9f561e116012
Denis <denis.gojkovic@gmail.com>
2024-10-13 18:00:44 +0200

    first commit

---
+++ schemaTypes/heroImages.ts
@@ -0,0 +1,42 @@
+export default {
+    name: "heroImag",
+    type: "document",
+    title: "Two Hero Images",
+    fields: [
+        {
+            name: "image1",
+            type: "image",
+            title: "First Image",
+        },
+        {
+            name: "text1",
+            type: "string", // You can change this to "text" for multiline
+            title: "Text for First Image",
+        },
+        {
+            name: "image2",
+            type: "image",
+            title: "Second Image",
+        },
+        {
+            name: "text2",
+            type: "string", // You can change this to "text" for multiline
+            title: "Text for Second Image",
+        },
+        {
+            name: "text02",
+            type: "string", // You can change this to "text" for multiline
+            title: "smaller Text for Second Image",
+        },
+        {
+            name: "image3",
+            type: "image",
+            title: "Third Image",
+        },
+        {
+            name: "text3",
+            type: "string", // You can change this to "text" for multiline
+            title: "Text for Third Image",
+        },
+    ],
+};
---
+++ schemaTypes/index.ts
@@ -0,0 +1,5 @@
+import category from "./category";
+import heroImage from "./heroImages";
+import product from "./product";
+
+export const schemaTypes = [product,category,heroImage]
